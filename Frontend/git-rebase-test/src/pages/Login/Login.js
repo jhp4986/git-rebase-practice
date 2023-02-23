b67444a8c7888f77d8dd642c402a8e2cd9b94d7b
jacob Park <jacobpark@jacobui-MacBookPro.local>
2023-02-23 19:51:29 +0900

    Revert " conflict 해결"
    This reverts commit e23fcdc12f92726b60f029b12f1e3a56957417f9.

--- Frontend/git-rebase-test/src/pages/Login/Login.js
+++ Frontend/git-rebase-test/src/pages/Login/Login.js
@@ -1,49 +1,9 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import React from 'react'
 
-export default function Login() {
-  const [userInfo, setUserInfo] = useState({ id: "", password: "" });
-  const navigate = useNavigate();
-
-  const getUserInfo = (e) => {
-    const { name, value } = e.target;
-    setUserInfo({ ...userInfo, [name]: value });
-  };
-
-  const isValid = userInfo.id.includes("@") && userInfo.password.length >= 5;
-
-  const goToMain = (e) => {
-    e.preventDefault();
-    navigate("/main");
-  };
-
+function Login() {
   return (
-    <div className="login">
-      <main className="loginContainer">
-        <h1 className="title">Git Rebase Test</h1>
-        <form className="loginForm" onSubmit={goToMain}>
-          <input
-            className="userId"
-            name="id"
-            type="text"
-            value={userInfo.id}
-            placeholder="이메일을 입력해 주세요."
-            onChange={getUserInfo}
-          />
-          <input
-            className="userPassword"
-            name="password"
-            type="password"
-            value={userInfo.password}
-            placeholder="비밀번호를 입력해 주세요."
-            onChange={getUserInfo}
-          />
-          <button className="loginButton" type="button" disabled={!isValid}>
-            로그인
-          </button>
-        </form>
-      </main>
-    </div>
-  );
+    <div>Login</div>
+  )
 }
+
+export default Login
